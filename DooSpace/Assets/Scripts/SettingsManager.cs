24daf748bbfab04e4ff83c728c749ab7fc53d20b
Bastien <bigot.bast@gmail.com>
2021-06-11 15:26:52 +0200

    settings 1/?

---
+++ DooSpace/Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsManager : MonoBehaviour
+{
+    [SerializeField] private RectTransform extraSoundRect;
+    [SerializeField] private RectTransform extraFlagRect;
+
+    Vector3 startExtraPos;
+    Vector3 endExtraPos;
+
+    void Start()
+    {
+        //startExtraPos = 
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OpenExtraSound()
+	{
+
+	}
+
+    public void OpenExtraFlag()
+    {
+
+    }
+}
