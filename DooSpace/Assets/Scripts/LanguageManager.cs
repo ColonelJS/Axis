01eb648b000afb2e1f64462d583af5cf62d21c9f
Bastien <bigot.bast@gmail.com>
2021-08-08 20:21:52 +0200

    how to play screen

--- DooSpace/Assets/Scripts/LanguageManager.cs
+++ DooSpace/Assets/Scripts/LanguageManager.cs
@@ -11,9 +11,13 @@ public class LanguageManager : MonoBehaviour
     [SerializeField] Text txtPlay;
     [SerializeField] Text txtUpgrade;
     [SerializeField] Text txtWatchAds;
+    [SerializeField] Text txtHowToPlay;
+    [SerializeField] GameObject tutoScrollView;
 
     public string language = "";
     public string[] elementInfo;
+
+    Text[] listTutoItemText;
 
     private void Awake()
 	{
@@ -26,7 +30,7 @@ public class LanguageManager : MonoBehaviour
 	}
 
 	void Start()
-    {      
+    {
 
     }
 
@@ -69,6 +73,7 @@ public class LanguageManager : MonoBehaviour
     void SetupElementInfo()
     {
         elementInfo = new string[3];
+        listTutoItemText = tutoScrollView.GetComponentsInChildren<Text>();
         if (language == "fr")
         {
             elementInfo[0] = "Augmente la vitesse de la fusée (+25%)";
@@ -102,6 +107,23 @@ public class LanguageManager : MonoBehaviour
             elementInfo[2] = "Diminue le taux de carburant perdu au contacte des météorites";
 
             txtWatchAds.text = "Regarder une pub pour argent x2";
+
+            txtHowToPlay.text = "Comment jouer";
+
+            listTutoItemText[0].text = "item 1 fr";
+            listTutoItemText[1].text = "item 1 desc fr";
+
+            listTutoItemText[2].text = "item 2 fr";
+            listTutoItemText[3].text = "item 2 desc fr";
+
+            listTutoItemText[4].text = "item 3 fr";
+            listTutoItemText[5].text = "item 3 desc fr";
+
+            listTutoItemText[6].text = "item 4 fr";
+            listTutoItemText[7].text = "item 4 desc fr";
+
+            listTutoItemText[8].text = "item 5 fr";
+            listTutoItemText[9].text = "item 5 desc fr";
         }
         else if(language == "en")
 		{
@@ -118,6 +140,23 @@ public class LanguageManager : MonoBehaviour
             elementInfo[2] = "Decreases the amount of fuel lost due to meteorites"; //bumper
 
             txtWatchAds.text = "Watch ads for money x2";
+
+            txtHowToPlay.text = "How to play";
+
+            listTutoItemText[0].text = "item 1 en";
+            listTutoItemText[1].text = "item 1 desc en";
+
+            listTutoItemText[2].text = "item 2 en";
+            listTutoItemText[3].text = "item 2 desc en";
+
+            listTutoItemText[4].text = "item 3 en";
+            listTutoItemText[5].text = "item 3 desc en";
+
+            listTutoItemText[6].text = "item 4 en";
+            listTutoItemText[7].text = "item 4 desc en";
+
+            listTutoItemText[8].text = "item 5 en";
+            listTutoItemText[9].text = "item 5 desc en";
         }
 	}
 }
